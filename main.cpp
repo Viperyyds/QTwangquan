2f42056d53be88a920292da07a103b680381feb9
Viperyyds <92869907+Viperyyds@users.noreply.github.com>
2022-04-05 16:49:36 +0800

    opengl3.29

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
-#include "udpwidget.h"
+
+#include"myglwidget.h"
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    UdpWidget w;
+    MyGLWidget w;
+    w.resize(400,300);
     w.show();
     return a.exec();
 }
